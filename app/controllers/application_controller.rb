5ca5e73982d8037545201946e4c1a3e4d937347b
Hey-Feres <brunoferesvillela@icloud.com>
2019-08-08 16:14:52 -0300

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+	StockQuote::Stock.new(api_key: "pk_8f2be937bfe54f46b6de402e11a4eff7")
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+	@stocks = StockQuote::Stock.quote("aapl, tsla, sbux, nke, cnk, mcd, amzn, fb, msft")  	
   end
 end
